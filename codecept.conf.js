cd098a66b0edba6067fbdcb5dbd1ea138f652716
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2022-11-10 15:46:02 -0300

    initial test-config

---
+++ codecept.conf.js
@@ -0,0 +1,20 @@
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Appium: {
+      platform: 'Android',
+      device: 'emulator-5554',
+      desiredCapabilities: {
+        automationName: 'UiAutomator2',
+        appPackage: 'com.starkixltd',  // Package name of your app
+        appActivity: 'com.starkixltd.MainActivity', // App activity of the app
+      }
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'startKix-front'
+}
