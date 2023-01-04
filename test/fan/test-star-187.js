94f2d8adcdf87ffdf1b2d90825a83a9f309eca15
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2023-01-04 17:02:14 -0300

    new move

--- test/fan/test-star-187.js
+++ test/fan/test-star-187.js
@@ -1,8 +1,9 @@
-const email = '2225@gmail.com'
+const email = 'za@nonerds.com'
 Feature('Recovery Password');
 
 Scenario('As a Fan, I want to be able to recover my password', ({ I }) => {
-    I.wait(10);
+    //solo se llega hasta el envio de la solicitud
+    I.wait(5);
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.Button/android.widget.TextView' });
     I.wait(2);
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[7]/android.widget.TextView' })
@@ -10,5 +11,8 @@ Scenario('As a Fan, I want to be able to recover my password', ({ I }) => {
     I.fillField('#RNE__Input__text-input', email);
     I.tap({ xpath: '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.Button' });
     I.wait(3);
+    I.tap('#accept_button_modal');
+    I.wait(3);
+
     pause();
 });
