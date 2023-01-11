581a18afc60154a2a8dead0cae14713ce304bb33
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2023-01-11 15:34:58 -0300

    resolved appTester staging issue

--- codecept.conf.js
+++ codecept.conf.js
@@ -8,6 +8,7 @@ exports.config = {
       device: 'emulator-5554',
       desiredCapabilities: {
         automationName: 'UiAutomator2',
+        platformVersion: "12.0",
         appPackage: 'com.starkixltd.staging',  // Package name of your app
         appActivity: 'com.starkixltd.MainActivity', // App activity of the app
       }
